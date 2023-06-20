a26bad08d5a5690012eea3b9ae4d44e9edff3089
dev-marko <markospasenovski00@gmail.com>
2023-06-21 00:03:57 +0200

    Implement Text size changer logic

--- src/components/DisplayPreferences/FontSizeSlider.tsx
+++ src/components/DisplayPreferences/FontSizeSlider.tsx
@@ -5,17 +5,61 @@ import {
   SliderMark,
   SliderThumb,
   SliderTrack,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
+
+import { FONT_SIZE_KEY } from '@/constants/theme-constants';
+import useLocalStorage from '@/hooks/useLocalStorage';
 
 const FontSizeSlider = () => {
+  const [fontSize, setFontSize] = useLocalStorage(FONT_SIZE_KEY, {
+    numericValue: 40,
+    stringValue: 'md',
+  });
+
+  const handleChange = (value: number) => {
+    switch (value) {
+      case 20:
+        setFontSize({
+          numericValue: 20,
+          stringValue: 'sm',
+        });
+        break;
+      case 40:
+        setFontSize({
+          numericValue: 40,
+          stringValue: 'md',
+        });
+        break;
+      case 60:
+        setFontSize({
+          numericValue: 60,
+          stringValue: 'lg',
+        });
+        break;
+      case 80:
+        setFontSize({
+          numericValue: 80,
+          stringValue: 'xl',
+        });
+        break;
+      default:
+        setFontSize({
+          numericValue: 40,
+          stringValue: 'md',
+        });
+        break;
+    }
+  };
+
   return (
     <Slider
-      defaultValue={40}
+      defaultValue={fontSize.numericValue}
       min={0}
       max={100}
       step={20}
-      w={{ base: "100%", md: "75%" }}
-      alignSelf={"center"}
+      w={{ base: '100%', md: '75%' }}
+      alignSelf={'center'}
+      onChange={handleChange}
     >
       <Box marginY={5}>
         <SliderMark value={20}>S</SliderMark>
@@ -23,7 +67,7 @@ const FontSizeSlider = () => {
         <SliderMark value={60}>L</SliderMark>
         <SliderMark value={80}>XL</SliderMark>
       </Box>
-      <SliderTrack bg={"primary.100"}>
+      <SliderTrack bg={'primary.100'}>
         <SliderFilledTrack />
       </SliderTrack>
       <SliderThumb boxSize={6} paddingBottom={0} />
