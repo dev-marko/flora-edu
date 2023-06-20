07d376d6780b8c1cbe503d25a5e762449c8345df
dev-marko <markospasenovski00@gmail.com>
2023-06-21 00:01:15 +0200

    Add path aliasing for hooks folder

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@components": `${path.resolve(__dirname, "./src/components")}`,
+      "@hooks": `${path.resolve(__dirname, "./src/hooks")}`,
       "@pages": path.resolve(__dirname, "./src/pages"),
       "@public": `${path.resolve(__dirname, "./public")}`,
       "@themes": `${path.resolve(__dirname, "./src/styles/themes")}`,
