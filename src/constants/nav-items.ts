a7a789ec88f255c471656faa8bfec57b3e910743
dev-marko <markospasenovski00@gmail.com>
2023-06-25 00:29:42 +0200

    Add nav item interface and constants

---
+++ src/constants/nav-items.ts
@@ -0,0 +1,20 @@
+import { NavItem } from '@/interfaces/nav-item';
+
+export const NAV_ITEMS: Array<NavItem> = [
+  {
+    label: 'Inspiration',
+    href: '#',
+  },
+  {
+    label: 'Find Work',
+    href: '#',
+  },
+  {
+    label: 'Learn Design',
+    href: '#',
+  },
+  {
+    label: 'Hire Designers',
+    href: '#',
+  },
+];
