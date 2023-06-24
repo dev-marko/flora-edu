7c6b12235e4937e6c1fe2b9fd69c15fe0cb831bb
dev-marko <markospasenovski00@gmail.com>
2023-06-24 12:41:22 +0200

    Initial HeroSection component

---
+++ src/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,15 @@
+import { Box, Flex } from '@chakra-ui/react';
+
+const HeroSection = () => {
+  return (
+    <Box bg={'gray'}>
+      <Flex>
+        <Box bg={'whiteAlpha.300'}>
+          text
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default HeroSection;
