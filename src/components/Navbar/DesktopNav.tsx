747f600e925cf63308f4d8442e3457ce21a35125
dev-marko <markospasenovski00@gmail.com>
2023-06-25 00:29:48 +0200

    Add desktop variant

---
+++ src/components/Navbar/DesktopNav.tsx
@@ -0,0 +1,40 @@
+import {
+  useColorModeValue,
+  Stack,
+  Popover,
+  PopoverTrigger,
+  Link,
+  Box,
+} from '@chakra-ui/react';
+import { NAV_ITEMS } from '@/constants/nav-items';
+
+export const DesktopNav = () => {
+  const linkColor = useColorModeValue('gray.600', 'gray.200');
+  const linkHoverColor = useColorModeValue('gray.800', 'white');
+
+  return (
+    <Stack direction={'row'} spacing={4}>
+      {NAV_ITEMS.map((navItem) => (
+        <Box key={navItem.label}>
+          <Popover trigger={'hover'} placement={'bottom-start'}>
+            <PopoverTrigger>
+              <Link
+                p={2}
+                href={navItem.href ?? '#'}
+                fontSize={'sm'}
+                fontWeight={500}
+                color={linkColor}
+                _hover={{
+                  textDecoration: 'none',
+                  color: linkHoverColor,
+                }}
+              >
+                {navItem.label}
+              </Link>
+            </PopoverTrigger>
+          </Popover>
+        </Box>
+      ))}
+    </Stack>
+  );
+};
