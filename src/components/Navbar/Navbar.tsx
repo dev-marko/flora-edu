9aff372108b3ab69ca96990ae56096a72e9f963b
dev-marko <markospasenovski00@gmail.com>
2023-06-28 23:00:32 +0200

    Refactor Navbar alignment

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -38,11 +38,12 @@ const Navbar = ({ onOpenDisplayPreferencesCallback }: Props) => {
         align={'center'}
       >
         <Flex
-          flex={{ base: 1, md: 3, lg: 3, xl: 'auto' }}
-          ml={{ base: -2 }}
-          display={{ base: 'flex', md: 'flex', lg: 'flex', xl: 'none' }}
+          flex={1}
+          justify={{ base: 'space-between', md: 'space-between' }}
+          align={'center'}
         >
           <IconButton
+            display={{ base: 'flex', md: 'flex', lg: 'flex', xl: 'none' }}
             onClick={onToggle}
             icon={
               isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
@@ -50,12 +51,6 @@ const Navbar = ({ onOpenDisplayPreferencesCallback }: Props) => {
             variant={'ghost'}
             aria-label={'Toggle Navigation'}
           />
-        </Flex>
-        <Flex
-          flex={{ base: 1, md: 6 }}
-          justify={{ base: 'center', md: 'space-evenly' }}
-          align={'center'}
-        >
           <Heading
             textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
             color={theme.colors.primary[500]}
@@ -65,57 +60,48 @@ const Navbar = ({ onOpenDisplayPreferencesCallback }: Props) => {
             ФлораЕду
           </Heading>
 
-          <Flex
-            display={{ base: 'none', md: 'none', lg: 'none', xl: 'flex' }}
-            ml={10}
-          >
+          <Flex display={{ base: 'none', md: 'none', lg: 'none', xl: 'flex' }}>
             <DesktopNav />
           </Flex>
+          <Stack justify={'start'} direction={'row'} spacing={2}>
+            <Button
+              as={'a'}
+              variant={'link'}
+              size={{ base: 'xs', md: 'md', lg: 'lg', xl: 'sm' }}
+              href={'#'}
+            >
+              Најава
+            </Button>
+            <Button
+              as={'a'}
+              display={{
+                base: 'none',
+                md: 'none',
+                lg: 'none',
+                xl: 'inline-flex',
+              }}
+              size={'sm'}
+              href={'#'}
+            >
+              Регистрација
+            </Button>
+            <Button
+              onClick={onOpenDisplayPreferencesCallback}
+              size={'sm'}
+              display={{
+                base: 'none',
+                md: 'none',
+                lg: 'none',
+                xl: 'inline-flex',
+              }}
+            >
+              <HStack>
+                <SettingsIcon></SettingsIcon>
+                <Text fontSize={'sm'}>Поставки</Text>
+              </HStack>
+            </Button>
+          </Stack>
         </Flex>
-
-        <Stack
-          flex={{ base: 1, md: 3, lg: 3, xl: 0 }}
-          justify={'flex-end'}
-          direction={'row'}
-          spacing={6}
-        >
-          <Button
-            as={'a'}
-            variant={'link'}
-            size={{ base: 'xs', md: 'md', lg: 'lg', xl: 'sm' }}
-            href={'#'}
-          >
-            Најава
-          </Button>
-          <Button
-            as={'a'}
-            display={{
-              base: 'none',
-              md: 'none',
-              lg: 'none',
-              xl: 'inline-flex',
-            }}
-            size={'sm'}
-            href={'#'}
-          >
-            Регистрација
-          </Button>
-          <Button
-            onClick={onOpenDisplayPreferencesCallback}
-            size={'sm'}
-            display={{
-              base: 'none',
-              md: 'none',
-              lg: 'none',
-              xl: 'inline-flex',
-            }}
-          >
-            <HStack>
-              <SettingsIcon></SettingsIcon>
-              <Text fontSize={'sm'}>Поставки</Text>
-            </HStack>
-          </Button>
-        </Stack>
       </Flex>
 
       <Collapse in={isOpen} animateOpacity endingHeight={'45em'}>
