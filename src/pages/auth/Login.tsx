1dc536ae5302c355b74fb07fab6fba7dc8a8c404
dev-marko <markospasenovski00@gmail.com>
2023-11-26 00:15:42 +0100

    Add logic to Login form

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -15,12 +15,23 @@ import {
   Divider,
   AbsoluteCenter,
   Link as ChakraLink,
+  useToast,
 } from '@chakra-ui/react';
 
+import { LoginRequest } from '@/interfaces/auth/login-request';
+
 import { useColorModeValue } from '@chakra-ui/system';
-import { Field, Formik, FormikProps } from 'formik';
+import { Field, Form, Formik, FormikHelpers, FormikProps } from 'formik';
 import { Facebook, Google } from 'react-bootstrap-icons';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import authService from '@/services/auth-service';
+import { LoginResponse } from '@/interfaces/auth/login-response';
+import useLocalStorage from '@/hooks/useLocalStorage';
+import { JWT_TOKEN_KEY } from '@/constants/local-storage-keys';
+import { ProblemDetails } from '@/interfaces/error/problem-details';
+import { ErrorCodes } from '@/enum/error-codes';
+import { CustomAxiosError } from '@/interfaces/error/custom-axios-error';
+import errorCodeMessages from '@/utils/error-code-translator';
 
 type LoginFormInputs = {
   username: string;
@@ -28,6 +39,10 @@ type LoginFormInputs = {
 };
 
 const Login = () => {
+  const toast = useToast();
+  const navigate = useNavigate();
+  const [jwtToken, setJwtToken] = useLocalStorage(JWT_TOKEN_KEY, {});
+
   const validateUsername = (username: string) => {
     let error;
     if (!username) {
@@ -38,14 +53,8 @@ const Login = () => {
 
   const validatePassword = (password: string) => {
     let error;
-    const passwordRegex = /(?=.*[0-9])/;
     if (!password) {
       error = 'Лозинка е задолжителна';
-    } else if (password.length < 12) {
-      error = 'Лозинката мора да е долга најмалку 12 карактери';
-    } else if (!passwordRegex.test(password)) {
-      error =
-        'Невалидна лозинка. Лозинката мора да содржи најмалку една бројка';
     }
     return error;
   };
@@ -99,99 +108,147 @@ const Login = () => {
             >
               <Formik
                 initialValues={initialValues}
-                onSubmit={async () => {
-                  console.log('alo');
+                onSubmit={async (
+                  values: LoginFormInputs,
+                  { setSubmitting, resetForm }: FormikHelpers<LoginFormInputs>
+                ) => {
+                  const loginRequest: LoginRequest = {
+                    username: values.username.trimStart().trimEnd(),
+                    password: values.password.trimStart().trimEnd(),
+                  };
+
+                  await authService
+                    .login(loginRequest)
+                    .then((response: LoginResponse) => {
+                      setJwtToken(response);
+                      setSubmitting(false);
+                      resetForm();
+                      navigate('/');
+                    })
+                    .catch((error: CustomAxiosError) => {
+                      const statusCode = error.axiosError.response?.status;
+                      const problemDetails = error.axiosError.response
+                        ?.data as ProblemDetails;
+                      const errorCode: ErrorCodes =
+                        ErrorCodes[
+                          problemDetails.title as keyof typeof ErrorCodes
+                        ];
+
+                      if (statusCode === 404) {
+                        toast({
+                          title: 'Настаната грешка.',
+                          description: errorCodeMessages.get(errorCode),
+                          status: 'error',
+                          duration: 5000,
+                          isClosable: true,
+                        });
+                      } else if (statusCode === 400) {
+                        toast({
+                          title: 'Настаната грешка.',
+                          description: errorCodeMessages.get(errorCode),
+                          status: 'error',
+                          duration: 5000,
+                          isClosable: true,
+                        });
+                      } else {
+                        error.handleGlobally && error.handleGlobally();
+                      }
+                    });
                 }}
               >
-                {(props: FormikProps<LoginFormInputs>) => (
-                  <Stack spacing={4}>
-                    <Field name="username" validate={validateUsername}>
-                      {({ field, form }: any) => (
-                        <FormControl
-                          isInvalid={
-                            form.errors.username && form.touched.username
-                          }
-                        >
-                          <FormLabel
-                            fontFamily={'Inter'}
-                            fontWeight={'semibold'}
+                {({ isSubmitting }: FormikProps<LoginFormInputs>) => (
+                  <Form>
+                    <Stack spacing={4}>
+                      <Field name="username" validate={validateUsername}>
+                        {({ field, form }: any) => (
+                          <FormControl
+                            isInvalid={
+                              form.errors.username && form.touched.username
+                            }
                           >
-                            Корисничко име
-                          </FormLabel>
-                          <Input
-                            {...field}
-                            fontFamily={'Inter'}
-                            type="text"
-                            name="username"
-                            placeholder="Вашето корисничко име..."
-                            autoComplete="off"
-                          />
-                          <FormErrorMessage>
-                            {form.errors.username}
-                          </FormErrorMessage>
-                        </FormControl>
-                      )}
-                    </Field>
-                    <Field name="password" validate={validatePassword}>
-                      {({ field, form }: any) => (
-                        <FormControl
-                          isInvalid={
-                            form.errors.password && form.touched.password
-                          }
-                        >
-                          <FormLabel
-                            fontFamily={'Inter'}
-                            fontWeight={'semibold'}
+                            <FormLabel
+                              fontFamily={'Inter'}
+                              fontWeight={'semibold'}
+                            >
+                              Корисничко име
+                            </FormLabel>
+                            <Input
+                              {...field}
+                              fontFamily={'Inter'}
+                              type="text"
+                              name="username"
+                              placeholder="Вашето корисничко име..."
+                              autoComplete="off"
+                            />
+                            <FormErrorMessage>
+                              {form.errors.username}
+                            </FormErrorMessage>
+                          </FormControl>
+                        )}
+                      </Field>
+                      <Field name="password" validate={validatePassword}>
+                        {({ field, form }: any) => (
+                          <FormControl
+                            isInvalid={
+                              form.errors.password && form.touched.password
+                            }
                           >
-                            Лозинка
-                          </FormLabel>
-                          <Input
-                            {...field}
+                            <FormLabel
+                              fontFamily={'Inter'}
+                              fontWeight={'semibold'}
+                            >
+                              Лозинка
+                            </FormLabel>
+                            <Input
+                              {...field}
+                              fontFamily={'Inter'}
+                              type="password"
+                              name="password"
+                              placeholder="Вашата тајна лозинка..."
+                              autoComplete="off"
+                            />
+                            <FormErrorMessage>
+                              {form.errors.password}
+                            </FormErrorMessage>
+                          </FormControl>
+                        )}
+                      </Field>
+                      <Stack spacing={8}>
+                        <Stack
+                          direction={{ base: 'column', sm: 'row' }}
+                          align={'start'}
+                          justify={'space-between'}
+                        >
+                          <Checkbox
+                            colorScheme={'green'}
                             fontFamily={'Inter'}
-                            type="password"
-                            name="password"
-                            placeholder="Вашата тајна лозинка..."
-                            autoComplete="off"
-                          />
-                          <FormErrorMessage>
-                            {form.errors.password}
-                          </FormErrorMessage>
-                        </FormControl>
-                      )}
-                    </Field>
-                    <Stack spacing={8}>
-                      <Stack
-                        direction={{ base: 'column', sm: 'row' }}
-                        align={'start'}
-                        justify={'space-between'}
-                      >
-                        <Checkbox
-                          colorScheme={'green'}
-                          fontFamily={'Inter'}
-                          size={{ base: 'md', md: 'sm' }}
+                            size={{ base: 'md', md: 'sm' }}
+                          >
+                            Запомни ме
+                          </Checkbox>
+                          <Text
+                            color={'black'}
+                            fontFamily={'Inter'}
+                            fontSize={'sm'}
+                            decoration={'underline'}
+                          >
+                            Заборавена лозинка?
+                          </Text>
+                        </Stack>
+                        <Button
+                          color={'white'}
+                          bgColor={feGreen.colors.primary[400]}
+                          _hover={{
+                            bg: feGreen.colors.primary[500],
+                          }}
+                          isLoading={isSubmitting}
+                          type="submit"
                         >
-                          Запомни ме
-                        </Checkbox>
-                        <Text
-                          color={'black'}
-                          fontFamily={'Inter'}
-                          fontSize={'sm'}
-                          decoration={'underline'}
-                        >
-                          Заборавена лозинка?
-                        </Text>
+                          Најави се
+                        </Button>
                       </Stack>
-                      <Button
-                        color={'white'}
-                        bgColor={feGreen.colors.primary[400]}
-                        _hover={{
-                          bg: feGreen.colors.primary[500],
-                        }}
-                      >
-                        Најави се
-                      </Button>
                     </Stack>
-                  </Stack>
+                  </Form>
                 )}
               </Formik>
               <Flex
