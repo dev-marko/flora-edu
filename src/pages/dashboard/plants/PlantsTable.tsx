3c8b255e2c9ed0610a163f4ee381e79b896694d5
dev-marko <markospasenovski00@gmail.com>
2023-12-10 23:00:55 +0100

    Create PlantsTable and PlantEditor

---
+++ src/pages/dashboard/plants/PlantsTable.tsx
@@ -0,0 +1,213 @@
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  Flex,
+  IconButton,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+  useDisclosure,
+  useToast,
+} from '@chakra-ui/react';
+import { AxiosResponse } from 'axios';
+import {
+  Await,
+  defer,
+  useLoaderData,
+  useNavigate,
+  useRevalidator,
+} from 'react-router-dom';
+
+import DashboardApi from '@/apis/dashboard-api';
+import { PlantsRequest } from '@/data/request-interfaces/plants-request';
+import LoadingSpinner from '@/components/shared/LoadingSpinner';
+import React, { useState } from 'react';
+import moment from 'moment';
+import { PlantTableData } from '@/data/interfaces/plant-table-data';
+import plantTypeTranslatorEngToMkd from '@/utils/plant-type-translator-eng-to-mkd';
+import { Pencil, Trash } from 'react-bootstrap-icons';
+
+type DeferData = {
+  plants: Promise<AxiosResponse>;
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export function loader() {
+  const requestDto: PlantsRequest = {
+    page: 1,
+    size: 5,
+  };
+
+  return defer({
+    plants: DashboardApi.getPlants(requestDto),
+  });
+}
+
+const PlantsTable = () => {
+  const dataPromise = useLoaderData() as DeferData;
+  const navigate = useNavigate();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = React.useRef();
+  const toast = useToast();
+  const revalidator = useRevalidator();
+  const [toDeleteId, setToDeleteId] = useState('');
+
+  const handleEditOnClick = (plantId: string) => {
+    navigate(`${plantId}`);
+  };
+
+  const handleDelete = () => {
+    DashboardApi.deletePlantById(toDeleteId)
+      .then(() => {
+        toast({
+          colorScheme: 'primary',
+          title: 'Успех',
+          description: 'Успешно избришано растение!',
+          status: 'success',
+          duration: 5000,
+          isClosable: true,
+        });
+        revalidator.revalidate();
+      })
+      .catch(() => {
+        toast({
+          title: 'Неуспех',
+          description: 'Неуспешна операција за бришење на растение!',
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+        });
+        revalidator.revalidate();
+      });
+  };
+
+  function renderPlants(axiosResponse: AxiosResponse) {
+    const plants = axiosResponse.data.items;
+
+    const plantCards = plants.map((plant: PlantTableData) => {
+      return (
+        <Tr>
+          <Td>{plant.name}</Td>
+          <Td>{plantTypeTranslatorEngToMkd.get(plant.type.toString())}</Td>
+          <Td>{moment(plant.createdAt).format('YYYY-MM-DD')}</Td>
+          <Td>{moment(plant.lastModified).format('YYYY-MM-DD')}</Td>
+          <Td>
+            <IconButton
+              aria-label={'Промени растение'}
+              icon={<Pencil />}
+              colorScheme={'primary'}
+              onClick={() => handleEditOnClick(plant.id)}
+            />
+          </Td>
+          <Td>
+            <IconButton
+              aria-label={'Избриши растение'}
+              icon={<Trash />}
+              colorScheme={'red'}
+              onClick={() => {
+                setToDeleteId(plant.id);
+                onOpen();
+              }}
+            />
+          </Td>
+        </Tr>
+      );
+    });
+
+    return plantCards;
+  }
+
+  return (
+    <Flex w={'full'} p={5}>
+      <TableContainer w={'full'} rounded={'lg'}>
+        <Table colorScheme="black" rounded={'lg'}>
+          <Thead bgColor={'gray.300'}>
+            <Tr>
+              <Th fontFamily={'Inter'} fontSize={'1rem'}>
+                Име
+              </Th>
+              <Th fontFamily={'Inter'} fontSize={'1rem'}>
+                Вид
+              </Th>
+              <Th fontFamily={'Inter'} fontSize={'1rem'}>
+                Креирано
+              </Th>
+              <Th fontFamily={'Inter'} fontSize={'1rem'}>
+                Модифицирано
+              </Th>
+              <Th fontFamily={'Inter'} fontSize={'1rem'}>
+                Промени
+              </Th>
+              <Th fontFamily={'Inter'} fontSize={'1rem'}>
+                Избриши
+              </Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <React.Suspense fallback={<LoadingSpinner />}>
+              <Await
+                resolve={dataPromise.plants}
+                errorElement={<p>Error loading plants data!</p>}
+              >
+                {renderPlants}
+              </Await>
+            </React.Suspense>
+          </Tbody>
+          <Tfoot bgColor={'gray.300'}>
+            <Tr>
+              <Th></Th>
+              <Th></Th>
+              <Th></Th>
+              <Th></Th>
+              <Th></Th>
+              <Th></Th>
+            </Tr>
+          </Tfoot>
+        </Table>
+      </TableContainer>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Избриши растение
+            </AlertDialogHeader>
+
+            <AlertDialogBody>Дали сте сигурни?</AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Откажи
+              </Button>
+              <Button
+                colorScheme="red"
+                onClick={() => {
+                  handleDelete();
+                  onClose();
+                }}
+                ml={3}
+              >
+                Избриши
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </Flex>
+  );
+};
+
+export default PlantsTable;
