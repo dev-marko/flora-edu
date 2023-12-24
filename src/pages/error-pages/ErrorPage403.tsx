cd24487c4df6c99c8d5e28be675a9b9c319c5622
dev-marko <markospasenovski00@gmail.com>
2023-12-25 00:36:02 +0100

    Refactor generic error pages to use Lottie files

--- src/pages/error-pages/ErrorPage403.tsx
+++ src/pages/error-pages/ErrorPage403.tsx
@@ -1,7 +1,20 @@
+import Lottie from 'lottie-react';
+import { Box, Container, Text, VStack } from '@chakra-ui/react';
+import forbiddenAnimation from '../../assets/lottie/403-forbidden.json';
+
 const ErrorPage403 = () => {
   return (
-    <h1>Error 403</h1>
+    <Container maxW={'container.xl'}>
+      <VStack w={'full'}>
+        <Box w={['20em', '30em']}>
+          <Lottie animationData={forbiddenAnimation} loop={true} />
+        </Box>
+        <Text fontSize={'2em'} fontWeight={'semibold'} textAlign={'center'}>
+          Грешка 403 - Несоодветна авторизација! Вратете се назад.
+        </Text>
+      </VStack>
+    </Container>
   );
-}
+};
 
 export default ErrorPage403;
